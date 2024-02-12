238f1181ca4e4ec3a4abb36b360a04deb1b2f3d3
Nicolas Palermo <h4ckcheek@gmail.com>
2024-02-12 10:31:50 -0300

    Fixed color bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,12 @@
-function Colors(color, bg)
-    bg = bg or true
-
+function Colors(color, not_bg)
 	color = color or 'rose-pine'
 	vim.cmd.colorscheme(color)
     
-    if not bg then
+    if not_bg then
         vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
         vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end
 end
 
 -- Default
-Colors('poimandres', false)
+Colors('poimandres', true)
